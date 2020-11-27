81279e3bdc12d0b88a09d3443c7085552424ec2b
KoennieTime <koenvandermarel@gmail.com>
2020-11-27 12:25:20 +0100

    Login panel beta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginPanelComponent } from './admin/login-panel/login-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginPanelComponent
   ],
   imports: [
     BrowserModule
