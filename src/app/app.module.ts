be9e412a459f88fea4e4a7f90bde46c0cb603b74
KoennieTime <koenvandermarel@gmail.com>
2020-11-27 14:51:41 +0100

    Klanten gedeelte ish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AdminNavigationComponent } from './admin-navigation/admin-navigation.component';
-import { AdminBreadcrumbComponent } from './admin-breadcrumb/admin-breadcrumb.component';
+import { AdminNavigationComponent } from './admin/admin-navigation/admin-navigation.component';
+import { AdminBreadcrumbComponent } from './admin/admin-breadcrumb/admin-breadcrumb.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginPanelComponent } from './admin/login-panel/login-panel.component';
+import { CustomerComponent } from './customer/customer.component';
+import { QuestionComponent } from './customer/question/question.component';
+import { CustomNavigationComponent } from './customer/question/custom-navigation/custom-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AdminComponent,
     AdminNavigationComponent,
-    AdminBreadcrumbComponent
+    AdminBreadcrumbComponent,
+    LoginPanelComponent,
+    CustomerComponent,
+    QuestionComponent,
+    CustomNavigationComponent
   ],
   imports: [
     BrowserModule
