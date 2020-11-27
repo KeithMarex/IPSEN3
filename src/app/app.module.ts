9282f363f7fdee22f434b1cca5be30fda2206893
JeroenVanVliet <j.van.vliet@hotmail.com>
2020-11-27 14:02:03 +0100

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminNavigationComponent } from './admin-navigation/admin-navigation.component';
+import { AdminBreadcrumbComponent } from './admin-breadcrumb/admin-breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminNavigationComponent,
+    AdminBreadcrumbComponent
   ],
   imports: [
     BrowserModule
