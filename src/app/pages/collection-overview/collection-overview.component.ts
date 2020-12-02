84d89ff3abf06020c3a561cf13d30138e32721ac
JeroenVanVliet <j.van.vliet@hotmail.com>
2020-12-02 11:50:48 +0100

    added new pages folder for routing

---
+++ src/app/pages/collection-overview/collection-overview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-overview',
+  templateUrl: './collection-overview.component.html',
+  styleUrls: ['./collection-overview.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class CollectionOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
