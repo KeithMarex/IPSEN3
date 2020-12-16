5c171292f48053c6534dbc5d9a2bd6014442b075
JeroenVanVliet <j.van.vliet@hotmail.com>
2020-12-16 14:59:54 +0100

    added new shit

---
+++ src/app/api/base-url.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://ipsen3api.nielsprins.com/'
+});
--- src/app/pages/collection-overview/collection-overview.component.ts
+++ src/app/pages/collection-overview/collection-overview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router, RouterModule } from "@angular/router";
+import api from '../../api/base-url';
 
 @Component({
   selector: 'app-collection-overview',
@@ -8,9 +11,23 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class CollectionOverviewComponent implements OnInit {
 
-  constructor() { }
+  collections = [{id: 'test', name: 'haha', type: 'concept', version: 1}];
+
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.getTestData();
   }
 
+  async getTestData() {
+    const response = await api.get('/collection/all');
+    this.convertDataToObject(response.data.result);
+  }
+
+  convertDataToObject(response) {
+    response.forEach(e => {
+      const row = { id: e.id, name: e.name, type: e.type, version: e.version }
+      this.collections.push(row);
+    });
+  }
 }
