a8fd08ddd50cdc839e769a4b0fc9b906aa9e9d62
KoennieTime <koenvandermarel@gmail.com>
2020-12-18 12:48:36 +0100

    Fix user log in

--- src/app/admin/login-panel/login-form/login-form.component.ts
+++ src/app/admin/login-panel/login-form/login-form.component.ts
@@ -34,7 +34,7 @@ export class LoginFormComponent implements OnInit {
       } else {
         console.log(responseData['result']['id']);
         const m = responseData['result'];
-        this.conf.user = new UserModel(m['id'], m['email'], m['permission_group'], m['voornaam'], m['achternaam']);
+        this.conf.user = new UserModel(m['id'], m['email'], m['permission_group'], m['first_name'], m['last_name']);
         console.log(this.conf.user);
         this.router.navigate([this.router.url + '/dashboard']);
       }
