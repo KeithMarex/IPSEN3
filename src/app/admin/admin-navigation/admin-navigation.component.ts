6ca9edc68e7d6231b7ee8e8fe9dbaee458ac88d8
KoennieTime <koenvandermarel@gmail.com>
2020-12-18 11:30:32 +0100

    Kijk maar op taiga

--- src/app/admin/admin-navigation/admin-navigation.component.ts
+++ src/app/admin/admin-navigation/admin-navigation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {configurationService} from "../../shared/configuration.service";
+import Swal from "sweetalert2";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-admin-navigation',
@@ -7,9 +10,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminNavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public conf: configurationService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  editAccount() {
+    Swal.mixin({
+      input: 'text',
+      confirmButtonText: 'Volgende &rarr;',
+      showCancelButton: true,
+      progressSteps: ['1', '2', '3']
+    }).queue([
+      {
+        title: 'Voer je oude wachtwoord in',
+      },
+      'Voer je nieuwe wachtwoord in',
+      'Herhaal je nieuwe wachtwoord'
+    ]).then((result) => {
+      // @ts-ignore
+      if (result.value) {
+        // @ts-ignore
+        const answers = JSON.parse(JSON.stringify(result.value))
+        // TODO add confirm previous password
+        if (answers[1] === answers[2]){
+          let postData = {id: this.conf.user.user_id, email: this.conf.user.email, password: answers[2], permission_group: this.conf.user.usergroup};
+          console.log(postData);
+          this.http.post('https://ipsen3api.nielsprins.com/user/update', postData).subscribe(responseData => {console.log(responseData)});
+          Swal.fire({
+            icon: "success",
+            title: 'Nieuw wachtwoord ingesteld!',
+            confirmButtonText: 'Oke'
+          })
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: 'Nieuwe wachtwoorden komen niet overeen',
+            text: 'Je nieuwe wachtwoord is niet opgeslagen.',
+            confirmButtonText: 'Oke'
+          })
+        }
+      }
+    })
+  }
+
+  createUser() {
+    Swal.mixin({
+      input: 'text',
+      confirmButtonText: 'Volgende &rarr;',
+      showCancelButton: true,
+      progressSteps: ['1', '2', '3', '4', '5']
+    }).queue([
+      {
+        title: 'Nieuwe gebruiker',
+        text: 'Geef een email-adres op'
+      },
+      {
+        title: 'Geef een wachtwoord op',
+      },
+      {
+        title: 'Geef de voornaam van de gebruiker',
+      },
+      {
+        title: 'Geef de achternaam van de gebruiker',
+      },
+      {
+        title: 'Geef de usergroup op',
+        text: 'super_admin of admin',
+      },
+    ]).then((result) => {
+      // @ts-ignore
+      if (result.value) {
+        // @ts-ignore
+        const answers = JSON.parse(JSON.stringify(result.value))
+        // TODO Check of er al een gebruiker bestaat met het opgegeven email adres -> Check wordt gedaan in API
+        let postData = {email: answers[0], first_name: answers[2], last_name: answers[3], password: answers[1], permission_group: answers[4]};
+        console.log(postData);
+        this.http.post('https://ipsen3api.nielsprins.com/user/create', postData).subscribe(responseData => {
+          console.log(responseData)
+          if (responseData['result'] === true){
+            Swal.fire({
+              icon: "success",
+              title: 'Gebruiker ' + answers[2] + ' aangemaakt!',
+              confirmButtonText: 'Oke'
+            })
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: 'Nieuwe gebruiker niet aangemaakt',
+              text: 'Het opgegeven email adres is al in gebruik.',
+              confirmButtonText: 'Oke'
+            })
+          }
+        });
+      }
+    })
+
+  }
 }
--- src/app/shared/configuration.service.ts
+++ src/app/shared/configuration.service.ts
@@ -4,4 +4,5 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class configurationService {
   user: UserModel;
+  baseURL: 'https://ipsen3api.nielsprins.com/'
 }
