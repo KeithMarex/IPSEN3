0f76fd7c268a03a18b4a433741317096ffa179fc
NielsPrins <NielsPrins@users.noreply.github.com>
2021-01-06 18:00:41 +0100

    Token

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -4,6 +4,6 @@ import {Cookie} from 'ng2-cookies/ng2-cookies';
 export default axios.create({
   baseURL: 'https://ipsen3api.nielsprins.com/',
   headers: {
-    Authorization: "Bearer " + Cookie.get('user_tokens'),
+    Authorization: "Bearer " + Cookie.get('user_token'),
   }
 });
