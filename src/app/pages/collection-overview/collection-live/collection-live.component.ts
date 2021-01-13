08a783ef86fa9028917b72083df9358b2485a90d
Carl <carlzee@live.nl>
2021-01-13 13:23:16 +0100

    Collection-live.component aangemaakt

---
+++ src/app/pages/collection-overview/collection-live/collection-live.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collection-live',
+  templateUrl: './collection-live.component.html',
+  styleUrls: ['./collection-live.component.scss']
+})
+export class CollectionLiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
