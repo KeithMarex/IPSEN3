092ba8768b7ea8182712c586d3b105c37fa678de
NielsPrins <NielsPrins@users.noreply.github.com>
2021-01-13 10:27:01 +0100

    Api

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -2,9 +2,9 @@ import axios, {AxiosInstance} from 'axios';
 import {Cookie} from 'ng2-cookies/ng2-cookies';
 
 export class Api {
-  private static apiServerUrl = 'http://localhost:3000/';
+  // private static apiServerUrl = 'http://localhost:3000/';
 
-  // private static apiServerUrl = 'https://ipsen3api.nielsprins.com/';
+  private static apiServerUrl = 'https://ipsen3api.nielsprins.com/';
 
   static getApi(): AxiosInstance {
     return axios.create({
