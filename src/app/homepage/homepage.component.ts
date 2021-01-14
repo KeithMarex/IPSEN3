8f7ba596ad6c7910954c1381f7a4b2b2de30e373
SS <xcyberxx@outlook.com>
2021-01-14 11:51:39 +0100

    Categorie selecteren functionaliteit
    Categorie func. Homepage.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -8,16 +8,40 @@ import {Router} from '@angular/router';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+  categories = [];
   collections = [];
+  selectedCategoryId : number;
 
   constructor(private router: Router) {
   }
 
   ngOnInit(): void {
+    this.loadCategories();
+    //this.loadCollections();
+  }
+
+  private loadCategories()
+  {
     const api = Api.getApi();
-    api.get('/collection/all').then((response) => {
-      this.collections = response.data.result;
+    api.get('/category/all').then(response => {
+      this.categories = response.data.result;
     });
+  }
+
+  private loadCollectionsByCat(catId : number) {
+    const api = Api.getApi();
+    api.get('/collection/all/' + catId).then((response) => {
+      this.collections = response.data.result;
+      console.log(response.data);
+    });
+  }
+
+  onSelectCategory(catId : number)
+  {
+    this.loadCollectionsByCat(catId);
+    console.log(catId);
+    this.selectedCategoryId = catId;
+
   }
 
   goToSelection(collectionID): void {
