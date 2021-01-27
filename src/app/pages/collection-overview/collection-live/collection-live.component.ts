d108706d0048becba5b63afdb09533f362baee03
Carl <carlzee@live.nl>
2021-01-27 10:15:06 +0100

    Niels api call get collection/getAllData/:collectionId gebruikt

--- src/app/pages/collection-overview/collection-live/collection-live.component.ts
+++ src/app/pages/collection-overview/collection-live/collection-live.component.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { customizeUtil, MindMapMain } from 'mind-map';
 import {Question} from '../../../shared/nodes/question.model';
-import {Api} from '../../../api/api';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Tree} from '../../../shared/nodes/tree.model';
 import {NodeModel} from '../../../shared/nodes/node.model';
 import {Answer} from '../../../shared/nodes/answer.model';
-import {ApiServiceModel} from "../../../shared/api-service/api-service.model";
+import {ApiServiceModel} from '../../../shared/api-service/api-service.model';
 
 const HIERARCHY_RULES = {
   ROOT: {
     name: 'Collection',
     backgroundColor: '#7EC6E1',
     getChildren: () => [
-      HIERARCHY_RULES.QUESTION,
-      HIERARCHY_RULES.ANSWERS,
-      HIERARCHY_RULES.NOTIFICATION
+      HIERARCHY_RULES.QUESTION
     ]
   },
   QUESTION: {
@@ -23,8 +20,7 @@ const HIERARCHY_RULES = {
     color: '#fff',
     backgroundColor: '#f4d03f',
     getChildren: () => [
-      HIERARCHY_RULES.ANSWERS,
-      HIERARCHY_RULES.NOTIFICATION
+      HIERARCHY_RULES.ANSWERS
     ]
   },
   ANSWERS: {
@@ -32,13 +28,22 @@ const HIERARCHY_RULES = {
     color: '#fff',
     backgroundColor: '#f9e79f',
     getChildren: () => [
-      HIERARCHY_RULES.NOTIFICATION
+      HIERARCHY_RULES.NOTIFICATION,
+      HIERARCHY_RULES.QUESTION
     ]
   },
   NOTIFICATION: {
     name: 'Notification',
     color: '#fff',
     backgroundColor: '#f5b7b1',
+    getChildren: () => [
+      HIERARCHY_RULES.END_NOTIFICATION
+    ]
+  },
+  END_NOTIFICATION: {
+    name: 'endNotification',
+    color: '#fff',
+    backgroundColor: '#f8f',
     getChildren: () => []
   }
 };
@@ -48,7 +53,7 @@ const option = {
   theme: 'normal',
   editable: true,
   selectable: false,
-  depth: 4,
+  depth: 6,
   hierarchyRule: HIERARCHY_RULES,
   enableDraggable: true,
 };
@@ -159,6 +164,8 @@ const mind = {
   }
 };
 
+const DROP_DOWN_STRING = 'DropDown';
+
 @Component({
   selector: 'app-root',
   templateUrl: './collection-live.component.html',
@@ -180,6 +187,7 @@ export class CollectionLiveComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     this.apiService = new ApiServiceModel();
+    // await this.setCollectionNameFromApi();
     await this.initialiseMindMapData().then(() => {
       this.mindMap = MindMapMain.show(option, this.mindMapData);
       this.setFirstQuestionMindMap();
@@ -231,8 +239,8 @@ export class CollectionLiveComponent implements OnInit {
     });
   }
 
-  async addNodesToTree(): Promise<void> {
-    await this.getNodesFromApi().then(nodes => {
+  async addNodesToTreeOld(): Promise<void> {
+    await this.getNodesFromApiOld().then(nodes => {
       for (const node of nodes) {
         this.tree.addNode(node);
       }
@@ -243,7 +251,9 @@ export class CollectionLiveComponent implements OnInit {
     const currentNode = this.mindMap.getNode(nodeId);
     const children = this.tree.getChildren(nodeId);
     for (const child of children) {
-      this.mindMap.addNode(currentNode, child.getId(), child.getText());
+      console.log('Adding child: ', child);
+      this.addNodeToMindMap(currentNode, child);
+      // this.mindMap.addNode(currentNode, child.getId(), child.getText());
       this.addAllChildrenToMindMap(child.getId());
     }
   }
@@ -262,19 +272,27 @@ export class CollectionLiveComponent implements OnInit {
     this.firstNodeId = firstQuestion.getId();
   }
 
+  addNodeToMindMap(parentNode: any, node: NodeModel): void {
+    this.mindMap.addNode(parentNode, node.getId(), node.getText());
+    this.mindMap.setNodeColor(node.getId(), node.getMindMapBackGroundColor(), node.getMindMapColor());
+  }
+
   async initialiseMindMapData(): Promise<void> {
     await this.initialiseTree().then(async r => {
-      await this.addNodesToTree().then(() => {
+      await this.addNodesToTreeFromApi().then(() => {
         this.mindMapData = this.tree.toMindMap();
       });
+      // await this.addNodesToTreeOld().then(() => {
+      //   this.mindMapData = this.tree.toMindMap();
+      // });
     });
   }
 
-  async getNodesFromApi(): Promise<Array<NodeModel>> {
+  async getNodesFromApiOld(): Promise<Array<NodeModel>> {
     const nodes: Array<NodeModel> = [];
     const firstQuestion = await this.getFirstQuestionFromApi();
     nodes.push(firstQuestion);
-    const nodes2 = await this.getAllNodesFromApi2(firstQuestion);
+    const nodes2 = await this.getAllNodesFromApiOld2(firstQuestion);
     for (const node2 of nodes2) {
       if (node2.getId() !== undefined) {
         nodes.push(node2);
@@ -283,14 +301,14 @@ export class CollectionLiveComponent implements OnInit {
     return nodes;
   }
 
-  async getAllNodesFromApi2(parentQuestion: Question): Promise<NodeModel[]> {
+  async getAllNodesFromApiOld2(parentQuestion: Question): Promise<NodeModel[]> {
     const nodes: Array<NodeModel> = [];
     const answers = await this.getAnswersFromApi(parentQuestion.getId());
     for (const answer of answers) {
       nodes.push(answer);
       const question = await this.getQuestionFromApi(answer.getId());
       nodes.push(question);
-      const children = await this.getAllNodesFromApi2(question);
+      const children = await this.getAllNodesFromApiOld2(question);
       for (const child of children) {
         nodes.push(child);
       }
@@ -301,7 +319,8 @@ export class CollectionLiveComponent implements OnInit {
   async getFirstQuestionFromApi(): Promise<Question> {
     let firstQuestion;
     const parentId = this.collectionId;
-    const questionType = 'DropDown'; // ToDo enumeration maken
+    // const questionType = Question.getDropDownTypeString();
+    const questionType = DROP_DOWN_STRING;
     await this.apiService.getFirstQuestionByCollectionId(this.collectionId).then((firstQuestionData) => {
       // @ts-ignore
       const questionId = firstQuestionData.id;
@@ -327,13 +346,62 @@ export class CollectionLiveComponent implements OnInit {
   }
 
   async getQuestionFromApi(answerId: string): Promise<Question> {
-    const path = '/question/getByAnswer/' + answerId;
     let question;
-    const questionType = 'DropDown';
+    // const questionType = Question.getDropDownTypeString();
+    const questionType = DROP_DOWN_STRING;
     await this.apiService.getQuestionByAnswerId(answerId).then((questionData) => {
       // @ts-ignore
       question = new Question(questionData.id, questionData.name, answerId, questionType);
     });
     return question;
+  }
+
+  async addNodesToTreeFromApi(): Promise<void> {
+    await this.apiService.getAllDataFromACollection(this.collectionId).then((allCollectionData) => {
+      this.extractFirstQuestion(allCollectionData);
+    });
+  }
+
+  extractFirstQuestion(allCollectionData: object): void {
+    // @ts-ignore
+    const refinedCollectionData = allCollectionData.question;
+    // @ts-ignore
+    const id = refinedCollectionData.id;
+    // @ts-ignore
+    const text = refinedCollectionData.name;
+    const parentId = this.collectionId;
+    const questionType = DROP_DOWN_STRING;
+    const firstQuestion = new Question(id, text, parentId, questionType);
+    this.tree.addNode(firstQuestion);
+    this.extractAnswers(refinedCollectionData);
+  }
+
+  extractAnswers(data: object): void {
+    // @ts-ignore
+    const answersData = data.answers;
+    if (answersData.length === 0) {
+      return;
+    }
+    // @ts-ignore
+    const parentId = data.id;
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < answersData.length; i++) {
+      const answer = new Answer(answersData[i].id, answersData[i].name, parentId);
+      this.tree.addNode(answer);
+      this.extractQuestions(answersData[i]);
+    }
+  }
+
+  extractQuestions(data: object): void {
+    // @ts-ignore
+    const questionData = data.question;
+    if (questionData === undefined) {
+      return;
+    }
+    const questionType = DROP_DOWN_STRING;
+    // @ts-ignore
+    const question = new Question(questionData.id, questionData.name, data.id, questionType);
+    this.tree.addNode(question);
+    this.extractAnswers(questionData);
   }
 }
